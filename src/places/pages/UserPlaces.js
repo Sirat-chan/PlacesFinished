d22fe37ddf06cbd657aa1e81fd5c1a4e76080052
Sirat-chan <sirat.benjemaa@edu.isetcom.tn>
2023-03-11 23:10:32 +0100

    Connect the backend with frontend

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import PlaceList from '../components/PlaceList';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import { useHttpClient } from '../../shared/hooks/http-hook';
+
+const UserPlaces = () => {
+  const [loadedPlaces, setLoadedPlaces] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+  const userId = useParams().userId;
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const responseData = await sendRequest(
+            `http://localhost:5000/api/places/user/${userId}`
+        );
+        setLoadedPlaces(responseData.places);
+      } catch (err) {}
+    };
+    fetchPlaces();
+  }, [sendRequest, userId]);
+
+  return (
+      <React.Fragment>
+        <ErrorModal error={error} onClear={clearError} />
+        {isLoading && (
+            <div className="center">
+              <LoadingSpinner />
+            </div>
+        )}
+        {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+      </React.Fragment>
+  );
+};
+
+export default UserPlaces;
